bb9cb9fc1b514ead7d0613a5c3fef38962f0eeeb
Evgeniy Ivanov <irelion_synergy@mail.ru>
2023-11-18 23:21:02 +0300

    P-0001: fix, pow

--- Synergy.py
+++ Synergy.py
@@ -4,7 +4,7 @@ fromNumber = int(input('от (включительно): '))
 toNumber = int(input('по (включительно): '))
 
 for i in range(fromNumber, toNumber):
-    numbers[f'{i}'] = i * i
+    numbers[f'{i}'] = pow(i, i)
 
 for key in numbers:
     value = numbers[key]
